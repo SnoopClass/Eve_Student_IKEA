0168ace9c3ce7f081141b5f548fed481294e0064
Seungwon <csw98213@gmail.com>
2022-10-02 17:57:21 +0900

    Initial Commit

---
+++ Eve_Student_IKEA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eve_Student_IKEA
+//
+//  Created by Seungwon Choi on 2022/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
